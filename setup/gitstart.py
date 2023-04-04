da084c1dfb60c34bc71bc44f5b12d138d7d31c6d
Som3ael7amaky <119911190+Som3ael7amaky@users.noreply.github.com>
2023-04-04 17:52:45 +0200

    Update gitstart.py

--- setup/gitstart.py
+++ setup/gitstart.py
@@ -5,7 +5,7 @@ OFFICIAL_UPSTREAM_REPO = sys.argv[1]
 ACTIVE_BRANCH_NAME = sys.argv[2]
 
 repo = Repo.init()
-origin = repo.create_remote("poname", OFFICIAL_UPSTREAM_REPO)
+origin = repo.create_remote("temponame", OFFICIAL_UPSTREAM_REPO)
 origin.fetch()
 repo.create_head(ACTIVE_BRANCH_NAME, origin.refs[ACTIVE_BRANCH_NAME])
 repo.heads[ACTIVE_BRANCH_NAME].checkout(True)
